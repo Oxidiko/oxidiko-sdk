fe5f40a43ce7b2600a51a3d8b3129268a6c8ee5c
Oxidiko <oxidiko@proton.me>
2025-07-09 15:39:34 +0200

    Update URL

--- oxidiko-sdk.js
+++ oxidiko-sdk.js
@@ -1,7 +1,7 @@
 // Oxidiko Web Vault JavaScript SDK (Secure postMessage flow)
 class OxidikoAuth {
     constructor(options = {}) {
-        this.baseUrl = options.baseUrl || 'https://oxidiko.vercel.app'
+        this.baseUrl = options.baseUrl || 'https://oxidiko.com'
         this.apiKey = options.apiKey || null
         this.popupWidth = options.popupWidth || 500
         this.popupHeight = options.popupHeight || 700
